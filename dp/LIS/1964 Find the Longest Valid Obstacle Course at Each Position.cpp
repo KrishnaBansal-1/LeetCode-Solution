f70becbb344ce5815ed63f28fac26bcf7dee9c69
KrishnaBansal-1 <krishnaserves01@gmail.com>
2026-07-28 23:08:46 +0530

    Create 1964 Find the Longest Valid Obstacle Course at Each Position.cpp

---
+++ dp/LIS/1964 Find the Longest Valid Obstacle Course at Each Position.cpp
@@ -0,0 +1,31 @@
+
+// You want to build some obstacle courses. You are given a 0-indexed integer array obstacles of length n, where obstacles[i] describes the height of the ith obstacle.
+
+// For every index i between 0 and n - 1 (inclusive), find the length of the longest obstacle course in obstacles such that:
+
+// You choose any number of obstacles between 0 and i inclusive.
+// You must include the ith obstacle in the course.
+// You must put the chosen obstacles in the same order as they appear in obstacles.
+// Every obstacle (except the first) is taller than or the same height as the obstacle immediately before it.
+// Return an array ans of length n, where ans[i] is the length of the longest obstacle course for index i as described above.
+
+
+class Solution {
+public:
+    vector<int> longestObstacleCourseAtEachPosition(vector<int>& obs) {
+        int n = obs.size();
+
+        vector<int> ans(n, 1);
+
+        vector<int> curlis(n, 1e9);
+
+        for (int i = 0; i < n; i++){
+            int idx = upper_bound(curlis.begin(), curlis.end(), obs[i]) - curlis.begin();
+
+            ans[i] = idx + 1;
+            curlis[idx] = obs[i];
+        }
+
+        return ans;
+    }
+};
