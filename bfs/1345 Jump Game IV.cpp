e16338dfa963c041e9b43ea040b56bb5d9a11f97
KrishnaBansal-1 <krishnaserves01@gmail.com>
2026-08-09 11:51:20 +0530

    Create 1345 Jump Game IV.cpp

---
+++ bfs/1345 Jump Game IV.cpp
@@ -0,0 +1,49 @@
+
+// Given an array of integers arr, you are initially positioned at the first index of the array.
+
+// In one step you can jump from index i to index:
+
+// i + 1 where: i + 1 < arr.length.
+// i - 1 where: i - 1 >= 0.
+// j where: arr[i] == arr[j] and i != j.
+// Return the minimum number of steps to reach the last index of the array.
+
+// Notice that you can not jump outside of the array at any time.
+
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+        int n=arr.size();
+        if(n==1)return 0;
+        unordered_map<int,vector<int>>mp;
+        for (int i=0;i<n;i++){
+            mp[arr[i]].push_back(i);
+        }
+        queue<pair<int,int>>q;
+        q.push({0,0});
+        vector<int>vis(n,0);
+        vis[0]=1;
+        while(!q.empty()){
+            int node=q.front().first;
+            int dist=q.front().second;
+            q.pop();
+            if(node==n-1)return dist;
+            if(node-1>=0 && !vis[node-1]){
+                vis[node-1]=1;
+                q.push({node-1,dist+1});
+            }
+            if(node+1<n && !vis[node+1]){
+                vis[node+1]=1;
+                q.push({node+1,dist+1});
+            }
+            for (int next:mp[arr[node]]){
+                if (!vis[next]){
+                    vis[next]=1;
+                    q.push({next,dist+1});
+                }
+            }
+            mp[arr[node]].clear();
+        }
+        return -1;
+    }
+};
