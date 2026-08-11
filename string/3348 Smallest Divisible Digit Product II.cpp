f302344e4ac85a5c3278a16b93b943c1922c8c8e
KrishnaBansal-1 <krishnaserves01@gmail.com>
2026-08-11 18:58:23 +0530

    Update 3348 Smallest Divisible Digit Product II.cpp

--- string/3348 Smallest Divisible Digit Product II.cpp
+++ string/3348 Smallest Divisible Digit Product II.cpp
@@ -12,7 +12,7 @@ public:
     ll gcd(ll a, ll b){
         return b == 0 ? a : gcd(b, a%b);
     }
-
+    // just find the smallest possible number (divisible by t) of  length 'len'
     string calc(ll t, int len){
         string ans;
 
@@ -38,16 +38,19 @@ public:
 
         string got = calc(t, n);
         // return got;
+
+//if size greater that is the ans
         if (got.size() > n || got == "-1" || got == s)
             return got;
-
+        
+        //avl[i] shows the product required when the first i character are taken
         vector<ll> avl(n+1, t);
         
-        int top = 0;
+        int top = 0; // get the first occuring zero
         for (; top < n; top++)
             if (s[top] == '0') break;
 
-        int i;
+        int i; //calculate the avl till top
         for (i = 1; i <= top; i++){
             if (s[i-1] == '0') break;
             avl[i] = avl[i-1] / gcd(avl[i-1], s[i-1]-'0');
@@ -57,17 +60,17 @@ public:
             return s;
 
         if (top == n) top--;
-
+        // now go from top to 0th index and check for each digit 
         for (i = top; i >= 0; i--){
-
+            //size remaining to the right side of i
             int rem = n - i - 1;
-
+            //for each increment and check if the product can be achievable
             for (int j = (s[i]-'0')+1; j <= 9; j++){
 
                 ll req = avl[i] / gcd(avl[i], j);
 
                 string got2 = calc(req, rem);
-
+                //if size at right enough then that is the answer
                 if (got2.size() == rem && got2 != "-1"){
                     string finalans = s.substr(0, i);
                     finalans.push_back('0'+j);
@@ -76,7 +79,7 @@ public:
                 }
             }
         }
-
+        //if finally no result found return with 1 greater length
         return calc(t, n+1);
     }
 };
