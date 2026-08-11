048aff25ece0c2b3d7a066b7c2af6485a5d37356
KrishnaBansal-1 <krishnaserves01@gmail.com>
2026-08-11 18:57:24 +0530

    Implement solution for smallest zero-free digit product

---
+++ string/3348 Smallest Divisible Digit Product II.cpp
@@ -0,0 +1,82 @@
+// You are given a string num which represents a positive integer, and an integer t.
+
+// A number is called zero-free if none of its digits are 0.
+
+// Return a string representing the smallest zero-free number greater than or equal to num such that the product of
+// its digits is divisible by t.
+// If no such number exists, return "-1".
+
+class Solution {
+public:
+    typedef long long ll;
+    ll gcd(ll a, ll b){
+        return b == 0 ? a : gcd(b, a%b);
+    }
+
+    string calc(ll t, int len){
+        string ans;
+
+        for (int i = 9; i > 1; i--){
+            while (t % i == 0){
+                ans.push_back('0' + i);
+                t /= i;
+            }
+        }
+
+        if (t > 1)
+            return "-1";
+
+        while (ans.size() < len)
+            ans.push_back('1');
+
+        reverse(ans.begin(), ans.end());
+        
+        return ans;
+    }
+    string smallestNumber(string s, long long t) {
+        int n = s.size();
+
+        string got = calc(t, n);
+        // return got;
+        if (got.size() > n || got == "-1" || got == s)
+            return got;
+
+        vector<ll> avl(n+1, t);
+        
+        int top = 0;
+        for (; top < n; top++)
+            if (s[top] == '0') break;
+
+        int i;
+        for (i = 1; i <= top; i++){
+            if (s[i-1] == '0') break;
+            avl[i] = avl[i-1] / gcd(avl[i-1], s[i-1]-'0');
+        }
+
+        if (avl[n] == 1)
+            return s;
+
+        if (top == n) top--;
+
+        for (i = top; i >= 0; i--){
+
+            int rem = n - i - 1;
+
+            for (int j = (s[i]-'0')+1; j <= 9; j++){
+
+                ll req = avl[i] / gcd(avl[i], j);
+
+                string got2 = calc(req, rem);
+
+                if (got2.size() == rem && got2 != "-1"){
+                    string finalans = s.substr(0, i);
+                    finalans.push_back('0'+j);
+                    finalans += got2;
+                    return finalans;
+                }
+            }
+        }
+
+        return calc(t, n+1);
+    }
+};
