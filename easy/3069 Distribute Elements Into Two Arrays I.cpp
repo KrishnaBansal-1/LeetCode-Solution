953b39fda9caa07379c79f0951b6d484663850f0
KrishnaBansal-1 <krishnaserves01@gmail.com>
2026-08-20 20:50:10 +0530

    Create 3069 Distribute Elements Into Two Arrays I.cpp

---
+++ easy/3069 Distribute Elements Into Two Arrays I.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> ar1;
+        vector<int> ar2;
+
+        ar1.push_back(nums[0]);
+        ar2.push_back(nums[1]);
+
+        for (int i = 2; i < n; i++){
+            if (ar1.back() > ar2.back())
+                ar1.push_back(nums[i]);
+            else 
+                ar2.push_back(nums[i]);
+        }
+
+        for (int i : ar2)
+            ar1.push_back(i);
+
+        return ar1;
+    }
+};
